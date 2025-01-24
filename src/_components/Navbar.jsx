6954d42ad59501418be5a354e252fb19c5f88450
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2025-01-24 15:07:30 -0800

    navbar changes

--- src/_components/Navbar.jsx
+++ src/_components/Navbar.jsx
@@ -2,13 +2,11 @@
 
 export default function Navbar() {
   return (
-    <nav className="flex gap-5 justify-end items-center px-6 py-3     bg-stone-300 amber-100-50stone-300 font-lora">
-  
-        <a href="#">Home</a>
+    <nav className="flex gap-10 justify-end items-center px-6 py-3  text-lg font-semibold    bg-stone-300 amber-100-50stone-300 font-lora">
+       <a href="#">Home</a>
         <a href="#services">Services</a>
         <a href="#about">About</a>
         <a href="#contact">Contact </a>
-      
   </nav>
   )
 }
